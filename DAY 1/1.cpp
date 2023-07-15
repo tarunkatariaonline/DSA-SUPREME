2a1485a6a8d35c5b37631c03a32c227827ace51a
tarunkatariaonline <tarunsoftsol@gmail.com>
2023-07-15 20:14:27 +0530

    first commit

--- DAY 1/1.cpp
+++ DAY 1/1.cpp
@@ -3,6 +3,49 @@ using namespace std;
 
 int main (){
 
-cout<<"Namaste Bharat.";
+// 1. printing namaste bharat
+
+// cout<<"Namaste Bharat.";
+
+
+
+// 2. input a number and print it
+
+// int a ;
+// cout<<"Enter a number :"<<endl;
+// cin>>a;
+// cout<<"value of a is : "<<a<<endl;
+
+
+
+// 3. types of data types in c++
+
+// int a =5;
+// bool flag =true;
+// float f = 2.4;
+// double d =3.5;
+// char c='A';
+
+
+// 4. sizeof oprator
+
+// int i =3;
+// bool flag = true;
+// cout<<sizeof(i)<<endl;
+// cout<<sizeof(flag)<<endl;
+
+
+// 5. basic oprators 
+
+// int a =5;
+// int b=2;
+
+// cout<<"a+b : "<<a+b<<endl;
+// cout<<"a-b : "<<a-b<<endl;
+// cout<<"a*b : "<<a*b<<endl;
+// cout<<"a/b : "<<a/b<<endl;
+// cout<<"a%b : "<<a%b<<endl;
+
+
     return 0;
 }
