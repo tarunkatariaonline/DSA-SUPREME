e2019addf038d65fe653fe4cbc8d40a6fabe1433
tarunkatariaonline <tarunsoftsol@gmail.com>
2023-07-19 22:26:19 +0530

    day 6

---
+++ DAY 6/vector.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    // vector<int>arr{2,4,5,6,7};
+    // vector<int>arr1(10,-1);
+    // vector<int>arr2;
+    // arr2.push_back(2);
+    // arr2.push_back(3);
+    // arr2.push_back(4);
+
+    // // for(auto i : arr){
+    // //     cout<<i<<endl;
+    // // }
+
+    // cout<<arr.size()<<endl;
+    // cout<<arr1.size()<<endl;
+    // cout<<arr2.size()<<endl;
+
+    // find unique element in an array :
+
+    // vector<int>arr{1,3,3,1,5,4,2,4,2};
+
+    // int ans = 0;
+
+    // for(auto i : arr){
+
+    //  ans = ans^i;
+    // }
+
+    // cout<<ans<<endl;
+
+    // union of two array
+    // vector<int> arr{1, 2, 3, 4, 5, 6};
+    // vector<int> arr2{7, 8, 9};
+
+    // vector<int> ans;
+
+    // for (auto i : arr)
+    // {
+    //     ans.push_back(i);
+    // }
+
+    // for (auto i : arr2)
+    // {
+    //     ans.push_back(i);
+    // }
+
+    // for (auto i : ans)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // inter section of two array
+    // vector<int> arr{1, 3, 5, 7};
+    // vector<int> arr2{2, 3, 4, 5};
+
+    // vector<int> ans;
+
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+
+    //     for (int j = 0; j < arr2.size(); j++)
+    //     {
+
+    //         if (arr[i] == arr2[j])
+    //         {
+    //             ans.push_back(arr[i]);
+    //         }
+    //     }
+    // }
+
+    // for (auto i : ans)
+    // {
+    //     cout << i << " ";
+    // }
+
+
+    //union without no repetation
+
+    
+    return 0;
+}
